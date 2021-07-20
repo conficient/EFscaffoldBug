bfa63cc1a2934fea5777b5b942538fb309252039
Howard Richards <dev@conficient.com>
2021-07-20 19:37:57 +0100

    Add project files.

---
+++ EFscaffoldBug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFscaffoldBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scaffold bug");
+        }
+    }
+}
